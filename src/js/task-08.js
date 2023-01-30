243a24cbede2141e44894f92be790a87334efab2
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-01-30 22:27:24 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,34 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    email: mail,
+    password,
+  };
+
+  if (mail === "" || password === "") {
+    alert("Fill all the fields of form");
+  } else {
+    console.log(formData);
+  }
+}
+
+// const formData = new FormData(event.currentTarget);
+
+// if (mail === "" || password === "") {
+//   alert("Fill all the fields of form");
+// } else {
+//   formData.forEach((value, key) => {
+//     console.log(`${key}: ${value} `);
+//   });
+// }
+// console.log(formData);
+// }

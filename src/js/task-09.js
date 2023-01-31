96d1ce622e370f258ed863d4f69207d051ee0077
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-01-31 11:44:06 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const buttonEl = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", changeBodyColorHandler);
+
+function changeBodyColorHandler() {
+  const colorEl = getRandomHexColor();
+  document.body.style.backgroundColor = colorEl;
+  textEl.textContent = colorEl;
 }

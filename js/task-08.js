1ab4ede41e59a7ac59fcedb9938fa48a0a94c88d
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-02-01 21:40:46 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,16 +5,16 @@ formRef.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
 
-  const formElements = event.currentTarget.elements;
-  const mail = formElements.email.value;
-  const password = formElements.password.value;
+  const { email, password } = event.currentTarget.elements;
+  const emailValue = email.value.trim();
+  const passwordValue = password.value;
 
   const formData = {
-    email: mail,
-    password,
+    email: emailValue,
+    password: passwordValue,
   };
 
-  if (mail === "" || password === "") {
+  if (emailValue === "" || passwordValue === "") {
     alert("Fill all the fields of form");
   } else {
     console.log(formData);
